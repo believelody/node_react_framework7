4c8f100a2538765bbb68ee2adec9f74fecdc2c3b
believelody <believelody@gmail.com>
2018-12-25 00:03:37 +0100

    Finishing app

--- searchController.js
+++ searchController.js
@@ -4,9 +4,10 @@ module.exports = (req, res) => {
   const { value } = req.query;
   const matches = [];
   if (value) {
+    console.log(value);
     countries
       .filter(country => country.name.toLowerCase().startsWith(value.toLowerCase()))
-      .map(country => matches.push(country));
+      .map(country => matches.push(country.name));
   }
 
   res.json(matches);
